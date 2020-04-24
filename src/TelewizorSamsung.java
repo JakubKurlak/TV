fa9e7d5df1a9612c1f611f4599eea38bee163ff7
Jakub Kurlak <j.a.kurlak@gmail.com>
2020-04-24 22:00:25 +0200

    finish abstract class TelewizorSamsung

--- src/TelewizorSamsung.java
+++ src/TelewizorSamsung.java
@@ -1,7 +1,42 @@
-public abstract class TelewizorSamsung implements Telewizor{
+public abstract class TelewizorSamsung implements Telewizor {
+
+    private boolean stanWlaczony = false;
+
+    private int numerKanalu = 1;
 
     @Override
     public void wlacz() {
+        stanWlaczony = true;
+        System.out.println("SAMSUNG TV");
+        wyswietlajObraz();
+    }
 
+    @Override
+    public void wylacz() {
+        System.out.println("ANIMACJA CZARNOŚCI");
+        stanWlaczony = false;
+    }
+
+    @Override
+    public void przelaczProgram(int numer) {
+        if (stanWlaczony) {
+            numerKanalu = numer;
+        }
+    }
+
+    private void wyswietlajObraz() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (stanWlaczony) {
+                    try {
+                        System.out.println("Wyświetlaj obraz: " + numerKanalu);
+                        Thread.sleep(3000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
     }
 }
